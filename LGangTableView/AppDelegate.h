afc78e1e124ff28763d0349752b38e9422a9b89f
liugang416 <18317770662@163.com>
2020-10-15 14:49:27 +0800

    tableView 封装 初始化项目

---
+++ LGangTableView/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  LGangTableView
+//
+//  Created by liugang on 2020/10/15.
+//  Copyright © 2020 liugang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic,strong) UIWindow *window;
+
+@end
+
---
+++ LGangTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGangTableView
+//
+//  Created by liugang on 2020/10/15.
+//  Copyright © 2020 liugang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
